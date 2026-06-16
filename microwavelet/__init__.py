aa3395b785ae82a97b881db1ad8a0a35fc757586
dylannpaterson <dylannpaterson@gmail.com>
2026-06-16 10:02:31 -0400

    chore: consolidate CUSUM workflow refinements into v6 branch

--- microwavelet/__init__.py
+++ microwavelet/__init__.py
@@ -69,7 +69,7 @@ Output schema
 """
 
 from ._core import analyze_lightcurve
-from ._cusum import find_anomalies_cusum, run_linear_cusum, run_quadratic_cusum, seed_by_flat_cusum
+from ._cusum import find_anomalies_cusum, run_linear_cusum, run_quadratic_cusum, seed_by_flat_cusum, run_backward_quadratic_cusum
 from ._fit import detect_anomalies_with_fit, get_paczynski
 from ._noise import characterize_noise, characterize_multiband_noise
 
